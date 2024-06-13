052d59c562bcf1a75608e6046ebf0c4e7d2af87e
sundharaj2517@gmail.com <sundharaj2517@gmail.com>
2024-06-13 21:35:54 +0530

    update

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
 
-const dashboard = () => {
+function UncontrolledExample() {
   return (
-    <div>
-      dashboard
-    </div>
-  )
+    <Tabs
+      defaultActiveKey="profile"
+      id="uncontrolled-tab-example"
+      className="mb-3"
+    >
+      <Tab eventKey="home" title="Home">
+        This My capstone MERN project
+      </Tab>
+      <Tab eventKey="profile" title="Profile">
+        My Github ID : Meenajayaraj
+
+      </Tab>
+      <Tab eventKey="contact" title="Contact" >
+        MailID : meenajayaraj18@gmail.com ||
+        linkedIN : Meena Jayaraj
+      </Tab>
+    </Tabs>
+  );
 }
 
-export default dashboard
+export default UncontrolledExample;
