ee5239b51dcdcb107a5fddd502ff8af80658e750
sundharaj2517@gmail.com <sundharaj2517@gmail.com>
2024-06-21 10:19:48 +0530

    UPDATES

--- src/services/helper.js
+++ src/services/helper.js
@@ -1,1 +1,2 @@
-export const BACKEND_URL = "http://localhost:4002";
+//export const BACKEND_URL = "http://localhost:4002";
+export const BACKEND_URL = "https://capstone-otp-manager-backend-7.onrender.com"
