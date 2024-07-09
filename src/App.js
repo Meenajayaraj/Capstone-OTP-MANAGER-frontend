1921b3a39ba441c88ec1d3e0da37d81829b2cbbe
sundharaj2517@gmail.com <sundharaj2517@gmail.com>
2024-07-09 10:52:24 +0530

    update

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Register from "./pages/register";
 import Dashboard from "./pages/dashboard";
 import Otp from "./pages/otp";
 import Error from "./pages/error";
-import passwordreset from "./components/Passwordreset";
-import forgotpassword from "./components/ForgotPassword";
+import Passwordreset from "./components/Passwordreset";
+import ForgotPassword from "./components/ForgotPassword";
 import Headers from "./components/headers";
 import { Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -19,8 +19,8 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/Passwordreset" element={<passwordreset />} />
-        <Route path="/ForgotPassword/:id/:token" element={<forgotpassword />} />
+        <Route path="/passwordreset" element={<Passwordreset />} />
+        <Route path="/forgotpassword/:id/:token" element={<ForgotPassword />} />
         <Route path="/user/otp" element={<Otp />} />
         <Route path="*" element={<Error />} />
       </Routes>
